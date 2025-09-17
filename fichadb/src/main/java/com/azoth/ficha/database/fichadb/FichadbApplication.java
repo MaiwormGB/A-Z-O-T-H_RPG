e64fb8a382f9fe71c01890f3cfe039b8cb4f6956
Maiworm <biel.maiworm@gmail.com>
2025-09-17 11:39:06 -0300

    Envio de arquivo para backend em java script
    Signed-off-by: Maiworm <biel.maiworm@gmail.com>

---
+++ fichadb/src/main/java/com/azoth/ficha/database/fichadb/FichadbApplication.java
@@ -0,0 +1,13 @@
+package com.azoth.ficha.database.fichadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FichadbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FichadbApplication.class, args);
+	}
+
+}
---
+++ fichadb/src/test/java/com/azoth/ficha/database/fichadb/FichadbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azoth.ficha.database.fichadb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FichadbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
